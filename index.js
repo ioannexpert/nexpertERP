148720657a7096452b9a790153415eab15d36e69
DanVasii <82170198+DanVasii@users.noreply.github.com>
2023-10-07 20:42:43 +0300

    login_page update and dashboard main

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mustache = require("mustache-express")
 
-app.use(express.static(path.join(__dirname, "pages")));
+app.engine('html', mustache());
+app.set('views', path.join(__dirname, '/pages'));
+
+
+app.use(express.static(path.join(__dirname, 'pages')));
 app.use("/assets",express.static(path.join(__dirname, "assets")));
 
 
 app.get("/",(req, res)=>{
-    res.render("/index.html");
+    res.render("index.html");
+})
+
+app.get("/dashboard",(req, res)=>{
+    res.render("dashboard.html");
 })
 
 app.listen(3000,()=>{
